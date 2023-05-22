7f7b2169368123232dbe2f992b17bdf27e849428
HenryFaul <rhfaul@gmail.com>
2023-05-22 19:34:46 +0200

    Initial commit

---
+++ resources/js/Components/ApplicationLogo.vue
@@ -0,0 +1,3 @@
+<template>
+    <img :src="$page.props.app_logo" alt="logo" style="width: 250px; height: 250px;">
+</template>
---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
+
+<template>
+    <div>
+        <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+            <ApplicationLogo class="block h-12 w-auto" />
+
+            <h1 class="mt-8 text-2xl font-medium text-gray-900">
+                Welcome to BaderCo.
+            </h1>
+
+            <p class="mt-6 text-gray-500 leading-relaxed">
+                BadgerCo makes managing projects effortless.
+            </p>
+        </div>
+
+        <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+
+        </div>
+    </div>
+</template>
